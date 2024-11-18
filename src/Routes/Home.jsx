07c1546ccb42ea7149dffb47a3ed7f935a4eae44
Jesús García Ortuño <jesuscnnbs@gmail.com>
2024-11-18 12:44:21 +0100

    add motion

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 import Layout from '../Layouts/Layout';
+import {motion} from 'framer-motion';
 
 export default function Home() {
   return (
     <Layout>
       <h1>Home</h1>
+      <motion.div className="card bg-primary size-20" animate={{ x: 100 }} />
     </Layout>
   )
 }
